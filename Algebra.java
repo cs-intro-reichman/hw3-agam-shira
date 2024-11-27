15f03e589c3be4bacf4d403ca1a3a8f3a5d5d029
Agam Yerucham <agamyerucham@Agams-MacBook-Pro.local>
2024-11-27 21:29:15 +0200

    finishef third task

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,110 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		while(x2 < 0) {
+			 result--;
+			 x2++;
+			 } 
+			 for(int i = 0; i < x2; i++) {
+				result++;
+        }
+		return result;
 	}
-
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		while(x2 < 0) {
+			result++; 
+			x2++;
+		/*
+		 * x1= 2 x2 = -3
+		 * x1 = 3  x2 = -2
+		 * x1 = 4 x2 = -1
+		 * x1 =5 x2 =0
+		 * 
+		 * x1 = -2 x2 = -3
+		 * x1 = -1 x2 = -2
+		 * x1 = 0 x2 = -1
+		 * x1 =1 x2 =0
+		 */
+		}
+		for (int i = 0; i < x2; i++) {
+			result--;
+			/*
+			 * x1 = -1
+			 * x2 =2 
+			 *  i =0
+			 * x1 = -2
+			 * i = 1
+			 * x1 = -3
+			 */
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		int convertToWhole = x2;
+		if (x2 < 0) {
+		convertToWhole = minus(0, x2);
+		}
+		for ( int i = 0 ; i < convertToWhole ; i++) {
+			result = plus(result, x1);
+		} if (x2 < 0) {
+			result =minus(0, result);
+		}
+		return result;	
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		if(n == 0)
+			return 1;
+		int counter = x;
+		for(int i = 1; i < n; i++){
+			x = times(x, counter);
+		}
+	    return x;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if(x1 < x2)
+			return 0;
+		int counter = 0;
+		while(x1 >= x2){
+			x1 = minus(x1, x2);
+			counter++;
+		}
+	    return counter;
 	}
 
 	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	public static int mod(int x1, int x2) { // 5 2
+		int division = div(x1, x2);
+		division = times(division, x2);
+		int modulo = minus(x1, division);
+		return modulo;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		if (x < 0) {
+			return -999;
+		} else {
+			int num = 0;
+			while (pow(num, 2) <= x) {
+				if(pow(num, 2) == x){
+					return num;
+				}
+				if (pow (plus(num, 1) , 2) > x) {
+					return num;
+				}
+			num++;
+		}
+		return num;
+	   }	  	  
+	}
 }
